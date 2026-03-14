af5a5463c6ee27f0779ffb2482d92b75fefe5231
NALLURI LOKESH BABU <138300460+nlokeshbabu1@users.noreply.github.com>
2026-03-14 13:21:35 +0530

    change the eks-add-on file

--- aws/eks-modules/modules/eks-add-on.tf
+++ aws/eks-modules/modules/eks-add-on.tf
@@ -1,13 +1,13 @@
-#resource "aws_eks_addon" "eks_addon_vpc_cni" {
-##  cluster_name = var.cluster_name
-# addon_name = "vpc-cni"
-#  addon_version = "v1.19.2-eksbuild.1"
-##  service_account_role_arn = aws_iam_role.eks-iam-role.arn
-#  tags = {
-#    Name = "eks_addon"
-#  }
+resource "aws_eks_addon" "eks_addon_vpc_cni" {
+  cluster_name = var.cluster_name
+  addon_name = "vpc-cni"
+  addon_version = "v1.19.2-eksbuild.1"
+  service_account_role_arn = aws_iam_role.eks-iam-role.arn
+  tags = {
+    Name = "eks_addon"
+  }
   
-#}
+}
 
 resource "aws_eks_addon" "eks_addon_kube_proxy" {
   depends_on = [ aws_eks_cluster.eks-cluster ]
@@ -32,7 +32,7 @@ resource "aws_eks_addon" "eks_addon_core_dns" {
   }
   
 }
-/*
+
 resource "aws_eks_addon" "eks_addon_ebs" {
   depends_on = [ aws_eks_cluster.eks-cluster ]
   cluster_name = var.cluster_name
@@ -56,6 +56,7 @@ resource "aws_eks_addon" "eks_addon_metric" {
   }
   
 }
-*/
+
+
 
 
