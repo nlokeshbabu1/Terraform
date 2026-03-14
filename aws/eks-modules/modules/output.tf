7ead4f8162bb341f0b13e51d47b7e4d76f0acec7
NALLURI LOKESH BABU <138300460+nlokeshbabu1@users.noreply.github.com>
2026-03-14 15:16:25 +0530

    Update EKS outputs to include IDs and ARNs

--- aws/eks-modules/modules/output.tf
+++ aws/eks-modules/modules/output.tf
@@ -1,26 +1,36 @@
 output "eks-cluster_name" {
   value = aws_eks_cluster.eks-cluster.name
-  
+
 }
 
 output "eks-cluster_version" {
   value = aws_eks_cluster.eks-cluster.version
-  
+
 }
 
 
 output "vpc_name" {
-  value = aws_vpc.eks-aws_vpc
-  
+  value = aws_vpc.eks-aws_vpc.id
+
 }
 
 output "vpc_cidr_block" {
   value = var.vpc_cidr_block
-  
+
 }
 
 
 output "public_subnet" {
-  value = aws_subnet.eks-public[count.index]
-  
+  value = aws_subnet.eks-public[*].id
+
+}
+
+output "cluster_arn" {
+  value = aws_eks_cluster.eks-cluster.arn
+
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.aws_eks_node_group.node_group_name
+
 }
