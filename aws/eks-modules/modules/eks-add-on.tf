5653799f0a35e35f164302e82476c190a1864269
NALLURI LOKESH BABU <138300460+nlokeshbabu1@users.noreply.github.com>
2026-03-14 16:28:12 +0530

    Refactor EKS add-on resources for clarity

--- aws/eks-modules/modules/eks-add-on.tf
+++ aws/eks-modules/modules/eks-add-on.tf
@@ -4,10 +4,6 @@ resource "aws_eks_addon" "eks_addon_vpc_cni" {
   addon_version = "v1.21.1-eksbuild.3"
 #  most_recent = true
   service_account_role_arn = aws_iam_role.eks-iam-role.arn
-  # Ensure node group is ready first
-  depends_on = [
-    aws_eks_node_group.aws_eks_node_group
-  ]
   tags = {
     Name = "eks_addon"
   }
@@ -21,10 +17,6 @@ resource "aws_eks_addon" "eks_addon_kube_proxy" {
   addon_version = "v1.34.3-eksbuild.2"
   #most_recent = true
   service_account_role_arn = aws_iam_role.eks-iam-role.arn
-  # Ensure node group is ready first
-  depends_on = [
-    aws_eks_node_group.aws_eks_node_group
-  ]
   tags = {
     Name = "eks_addon"
   }
@@ -38,10 +30,6 @@ resource "aws_eks_addon" "eks_addon_core_dns" {
   addon_version = "v1.13.2-eksbuild.1"
   #most_recent = true
   service_account_role_arn = aws_iam_role.eks-iam-role.arn
-  # Ensure node group is ready first
-  depends_on = [
-    aws_eks_node_group.aws_eks_node_group
-  ]
   tags = {
     Name = "eks_addon"
   }
@@ -55,10 +43,6 @@ resource "aws_eks_addon" "eks_addon_ebs" {
   addon_version = "v1.56.0-eksbuild.1"
   #most_recent = true
   service_account_role_arn = aws_iam_role.eks-iam-role.arn
-  # Ensure node group is ready first
-  depends_on = [
-    aws_eks_node_group.aws_eks_node_group
-  ]
   tags = {
     Name = "eks_addon"
   }
@@ -72,10 +56,6 @@ resource "aws_eks_addon" "eks_addon_metric" {
   addon_version = "v0.8.1-eksbuild.1"
   #most_recent = true
   service_account_role_arn = aws_iam_role.eks-iam-role.arn
-  # Ensure node group is ready first
-  depends_on = [
-    aws_eks_node_group.aws_eks_node_group
-  ]
   tags = {
     Name = "eks_addon"
   }
